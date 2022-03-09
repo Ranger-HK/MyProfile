32c1321a11b62eabf039610df22e98d5d9932039
Ranger-HK <maxravindu99@gmail.com>
2022-03-09 20:23:01 +0530

    bla bla

--- myprojects/spa/controler/customerController.js
+++ myprojects/spa/controler/customerController.js
@@ -1,94 +1,117 @@
-// customer
+// Customer
 
 // add customer
-$("#btnAdd").click(function (){
+$("#btnAdd").click(function () {
 
-    let customerId=$("#txtCusID").val();
-    let customerName=$("#txtCusName").val();
-    let customerAddress=$("#txtCusAddress").val();
-    let customerTP=$("#txtCusTP").val();
+    let customerId = $("#txtCusID").val();
+    let customerName = $("#txtCusName").val();
+    let customerAddress = $("#txtCusAddress").val();
+    let customerTP = $("#txtCusTP").val();
 
-   /* var customerOB={
-        id:customerId,
-        name:customerName,
-        address:customerAddress,
-        telephoneNumber:customerTP
-    };*/
 
-    var customerOB=new CustomerDTO(customerId,customerName,customerAddress,customerTP);
+    var customerOB = new CustomerDTO(customerId, customerName, customerAddress, customerTP);
 
     customerDB.push(customerOB);
-    clearFiled();
     loadAllCustomer();
+    clearField();
 
 
-    // textfiled click set
-    $("#tbltBody>tr").click(function (){
-        let customerId=$(this).children(":eq(0)").text();
-        let customerName=$(this).children(":eq(1)").text();
-        let customerAddress=$(this).children(":eq(2)").text();
-        let customerTP=$(this).children(":eq(3)").text();
+});
+
+// textfiled click set
+function setCustomer() {
+    $("#tbltBody>tr").click(function () {
+        let customerId = $(this).children(":eq(0)").text();
+        let customerName = $(this).children(":eq(1)").text();
+        let customerAddress = $(this).children(":eq(2)").text();
+        let customerTP = $(this).children(":eq(3)").text();
 
         $("#txtCusID").val(customerId);
         $("#txtCusName").val(customerName);
         $("#txtCusAddress").val(customerAddress);
         $("#txtCusTP").val(customerTP);
     });
+}
 
-
-});
 
 // table load
-function loadAllCustomer(){
+function loadAllCustomer() {
     $("#tbltBody").empty();
-    for (var i of customerDB){
+    for (var i of customerDB) {
         let row = `<tr><td>${i.getCustomerID()}</td><td>${i.getCustomerName()}</td><td>${i.getCustomerAddress()}</td><td>${i.getCustomerTelephoneNumber()}</td></tr>`
         $("#tbltBody").append(row);
+        setCustomer();
+        deleteCustomer();
 
     }
 }
-// ClearMethod
-function clearFiled(){
-    $("#txtCusID,#txtCusName,#txtCusAddress,#txtCusTP").val("");
+
+// customerDelete
+function deleteCustomer() {
+    $("#btnDelete").click(function () {
+        let getClickData = $("#txtCusID").val();
+        for (let i = 0; i < customerDB.length; i++) {
+            if (customerDB[i].getCustomerID() == getClickData) {
+                customerDB.splice(i, 1)
+            }
+        }
+        loadAllCustomer();
+        clearField();
+    });
 }
 
-$("#btnUpdate").click(function (){
-    let customerId=$("#txtCusID").val();
-    let customerName=$("#txtCusName").val();
-    let customerAddress=$("#txtCusAddress").val();
-    let customerTP=$("#txtCusTP").val();
 
-    for (var i=0; i < customerDB.length; i++){
-        customerDB[i].setCustomerID(customerId);
-        customerDB[i].setCustomerName(customerName);
-        customerDB[i].setCustomerAddress(customerAddress);
-        customerDB[i].setCustomerTelephoneNumber(customerTP);
+// customerUpdate
+$("#btnUpdate").click(function () {
+    let customerId = $("#txtCusID").val();
+    let customerName = $("#txtCusName").val();
+    let customerAddress = $("#txtCusAddress").val();
+    let customerTP = $("#txtCusTP").val();
+
+    for (var i = 0; i < customerDB.length; i++) {
+        if (customerDB[i].getCustomerID() == customerId) {
+            customerDB[i].setCustomerName(customerName);
+            customerDB[i].setCustomerAddress(customerAddress);
+            customerDB[i].setCustomerTelephoneNumber(customerTP);
+        }
+
     }
     loadAllCustomer();
+    clearField();
 });
 
+
 // searchCustomer
-$("#btnSearch").click(function (){
+$("#btnSearch").click(function () {
     var searchId = $("#txtSearch").val();
     var response = searchCustomer(searchId);
-    if (response){
+    if (response) {
         $("#txtCusID").val(response.getCustomerID());
         $("#txtCusName").val(response.getCustomerName());
         $("#txtCusAddress").val(response.getCustomerAddress());
         $("#txtCusTP").val(response.getCustomerTelephoneNumber());
-    }else {
+    } else {
         clearFiled();
         alert("Invalid Customer Name");
     }
 });
 
-function searchCustomer(id){
-    for (let i = 0 ; i < customerDB.length; i++){
-        if (customerDB[i].getCustomerID()==id){
+
+function searchCustomer(id) {
+    for (let i = 0; i < customerDB.length; i++) {
+        if (customerDB[i].getCustomerID() == id) {
             return customerDB[i];
         }
     }
 }
+
+
+// ClearMethod
+function clearField() {
+    $("#txtCusID,#txtCusName,#txtCusAddress,#txtCusTP").val("");
+}
+
+
 //text fields focusing
 $("#txtCusID").keydown(function (event) {
     if (event.key == "Enter") {
@@ -107,3 +130,10 @@ $("#txtCusAddress").keydown(function (event) {
         $("#txtCusTP").focus();
     }
 });
+
+$("#btnClear").click(function () {
+    clearField();
+});
+
+
+
